470b2ccf874ad13dacaf3193cb073cafc89915f2
Abeeku Bondzie <kwekubondzie@gmail.com>
2015-01-26 17:48:11 -0500

    Schema

--- db/schema.rb
+++ db/schema.rb
@@ -16,12 +16,7 @@ ActiveRecord::Schema.define(version: 20150125221428) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "artists", force: true do |t|
-    t.string   "name"
-    t.text     "bio"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+  
 
   create_table "critiques", force: true do |t|
     t.string   "song"
