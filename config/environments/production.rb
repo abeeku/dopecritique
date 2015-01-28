e4d96ead9bb36f34be5916d63e21395a5484138a
Abeeku Bondzie <kwekubondzie@gmail.com>
2015-01-28 09:18:27 -0500

    Add Paperclip S3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,6 +10,16 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
+config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['dopecritiqueassets'],
+    :access_key_id => ENV['AKIAIBPRHVSOQYLDGMGQ'],
+    :secret_access_key => ENV['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']
+  }
+}
+
+
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
