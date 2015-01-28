632a7f13d1edf73c27d0ec1ab5a4da53f2e3ca8f
Abeeku Bondzie <kwekubondzie@gmail.com>
2015-01-28 09:43:18 -0500

    mmmmm

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,14 @@ class User < ActiveRecord::Base
   acts_as_liker
   has_attached_file :avatar, :styles => { :medium => "300x300#", :thumb => "50x50#" },
   							:storage => :s3,
-  							:bucket => ENV['dopecritiqueassets']
+  							:bucket => ENV['dopecritiqueassets'],
+  							:s3_credentials => {
+                    :access_key_id => ENV['AKIAIBPRHVSOQYLDGMGQ'],
+                    :secret_access_key => ENV['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']
+                },
+                :s3_permissions => 'private',
+                :url => ":s3_domain_url",
+                :path => "/:class/avatars/:id_:basename.:style.:extension"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   validates_presence_of :full_name
 end
