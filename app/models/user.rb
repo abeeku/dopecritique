9344e34baea0044a19926e8044122a4dde2268e5
Abeeku Bondzie <kwekubondzie@gmail.com>
2015-01-28 09:35:36 -0500

    Add Bucket Option

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,9 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   has_many :critiques
   acts_as_liker
-  has_attached_file :avatar, :styles => { :medium => "300x300#", :thumb => "60x60#" }
+  has_attached_file :avatar, :styles => { :medium => "300x300#", :thumb => "50x50#" },
+  							:storage => :s3,
+  							:bucket => ENV['dopecritiqueassets']
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   validates_presence_of :full_name
 end
