028a3a2e7fd981d803d0b6666049b44989df78d7
Abeeku Bondzie <kwekubondzie@gmail.com>
2015-03-30 18:49:56 -0400

    Lazy

--- lib/paperclip/railtie.rb
+++ lib/paperclip/railtie.rb
@@ -1,7 +1,3 @@
-#Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
-#Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
-require 'paperclip'
-
 module Paperclip
   class MediaTypeSpoofDetector
     def type_from_file_command
